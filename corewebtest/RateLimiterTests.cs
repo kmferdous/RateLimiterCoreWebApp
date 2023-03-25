221bfb02bce5b921d316f2add76507b462ad2beb
FEJO-PC\feru0 <feru007@gmail.com>
2023-03-25 21:24:39 +0600

    folder rearrange

---
+++ corewebtest/RateLimiterTests.cs
@@ -0,0 +1,15 @@
+namespace corewebtest;
+
+public class RateLimiterTests
+{
+    [SetUp]
+    public void Setup()
+    {
+    }
+
+    [Test]
+    public void Test()
+    {
+        Assert.Pass();
+    }
+}
